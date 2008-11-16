b7702167de91bb0c0b8d51960d8996323b41fe7b
Waqas Hussain <waqas20@gmail.com>
2008-11-16 05:06:38 +0500

    Removed unused function send_to_session from sessionmanager (send_to_session replaced by session.send)

---
+++ core/sessionmanager.lua
@@ -0,0 +1,137 @@
+
+local tonumber, tostring = tonumber, tostring;
+local ipairs, pairs, print, next= ipairs, pairs, print, next;
+local collectgarbage = collectgarbage;
+local m_random = import("math", "random");
+local format = import("string", "format");
+
+local hosts = hosts;
+local sessions = sessions;
+
+local modulemanager = require "core.modulemanager";
+local log = require "util.logger".init("sessionmanager");
+local error = error;
+local uuid_generate = require "util.uuid".generate;
+local rm_load_roster = require "core.rostermanager".load_roster;
+
+local newproxy = newproxy;
+local getmetatable = getmetatable;
+
+module "sessionmanager"
+
+local open_sessions = 0;
+
+function new_session(conn)
+	local session = { conn = conn,  priority = 0, type = "c2s_unauthed" };
+	if true then
+		session.trace = newproxy(true);
+		getmetatable(session.trace).__gc = function () open_sessions = open_sessions - 1; print("Session got collected, now "..open_sessions.." sessions are allocated") end;
+	end
+	open_sessions = open_sessions + 1;
+	local w = conn.write;
+	session.send = function (t) w(tostring(t)); end
+	return session;
+end
+
+function destroy_session(session)
+	(session.log or log)("info", "Destroying session");
+	if session.host and session.username then
+		if session.resource then
+			hosts[session.host].sessions[session.username].sessions[session.resource] = nil;
+		end
+		if hosts[session.host] and hosts[session.host].sessions[session.username] then
+			if not next(hosts[session.host].sessions[session.username].sessions) then
+				log("debug", "All resources of %s are now offline", session.username);
+				hosts[session.host].sessions[session.username] = nil;
+			end
+		end
+	end
+	session.conn = nil;
+	session.disconnect = nil;
+	for k in pairs(session) do
+		if k ~= "trace" then
+			session[k] = nil;
+		end
+	end
+end
+
+function make_authenticated(session, username)
+	session.username = username;
+	if session.type == "c2s_unauthed" then
+		session.type = "c2s";
+	end
+	return true;
+end
+
+-- returns true, nil on success
+-- returns nil, err_type, err, err_message on failure
+function bind_resource(session, resource)
+	if not session.username then return nil, "auth", "not-authorized", "Cannot bind resource before authentication"; end
+	if session.resource then return nil, "cancel", "already-bound", "Cannot bind multiple resources on a single connection"; end
+	-- We don't support binding multiple resources
+
+	resource = resource or uuid_generate();
+	--FIXME: Randomly-generated resources must be unique per-user, and never conflict with existing
+	
+	if not hosts[session.host].sessions[session.username] then
+		hosts[session.host].sessions[session.username] = { sessions = {} };
+	else
+		if hosts[session.host].sessions[session.username].sessions[resource] then
+			-- Resource conflict
+			return nil, "cancel", "conflict", "Resource already exists"; -- TODO kick old resource
+		end
+	end
+	
+	session.resource = resource;
+	session.full_jid = session.username .. '@' .. session.host .. '/' .. resource;
+	hosts[session.host].sessions[session.username].sessions[resource] = session;
+	
+	session.roster = rm_load_roster(session.username, session.host);
+	
+	return true;
+end
+
+function streamopened(session, attr)
+						local send = session.send;
+						session.host = attr.to or error("Client failed to specify destination hostname");
+			                        session.version = tonumber(attr.version) or 0;
+			                        session.streamid = m_random(1000000, 99999999);
+			                        print(session, session.host, "Client opened stream");
+			                        send("<?xml version='1.0'?>");
+			                        send(format("<stream:stream xmlns='jabber:client' xmlns:stream='http://etherx.jabber.org/streams' id='%s' from='%s' version='1.0'>", session.streamid, session.host));
+						
+						local features = {};
+						modulemanager.fire_event("stream-features", session, features);
+						
+						send("<stream:features>");
+						
+						for _, feature in ipairs(features) do
+							send(tostring(feature));
+						end
+ 
+        			                send("</stream:features>");
+						log("info", "Stream opened successfully");
+						session.notopen = nil;
+end
+
+function send_to_available_resources(user, host, stanza)
+	local count = 0;
+	local to = stanza.attr.to;
+	stanza.attr.to = nil;
+	local h = hosts[host];
+	if h and h.type == "local" then
+		local u = h.sessions[user];
+		if u then
+			for k, session in pairs(u.sessions) do
+				if session.presence then
+					session.send(stanza);
+					count = count + 1;
+				end
+			end
+		end
+	end
+	stanza.attr.to = to;
+	return count;
+end
+
+return _M;
---
+++ plugins/mod_roster.lua
@@ -0,0 +1,93 @@
+
+local st = require "util.stanza"
+
+local jid_split = require "util.jid".split;
+local t_concat = table.concat;
+
+local rm_remove_from_roster = require "core.rostermanager".remove_from_roster;
+local rm_add_to_roster = require "core.rostermanager".add_to_roster;
+local rm_roster_push = require "core.rostermanager".roster_push;
+
+add_iq_handler("c2s", "jabber:iq:roster", 
+		function (session, stanza)
+			if stanza.tags[1].name == "query" then
+				if stanza.attr.type == "get" then
+					local roster = st.reply(stanza)
+								:query("jabber:iq:roster");
+					for jid in pairs(session.roster) do
+						if jid ~= "pending" then
+							roster:tag("item", {
+								jid = jid,
+								subscription = session.roster[jid].subscription,
+								ask = session.roster[jid].ask,
+								name = session.roster[jid].name,
+							});
+							for group in pairs(session.roster[jid].groups) do
+								roster:tag("group"):text(group):up();
+							end
+							roster:up(); -- move out from item
+						end
+					end
+					session.send(roster);
+					session.interested = true; -- resource is interested in roster updates
+					return true;
+				elseif stanza.attr.type == "set" then
+					local query = stanza.tags[1];
+					if #query.tags == 1 and query.tags[1].name == "item"
+							and query.tags[1].attr.xmlns == "jabber:iq:roster" and query.tags[1].attr.jid
+							and query.tags[1].attr.jid ~= "pending" then
+						local item = query.tags[1];
+						local from_node, from_host = jid_split(stanza.attr.from);
+						local node, host, resource = jid_split(item.attr.jid);
+						if not resource then
+							if item.attr.jid ~= from_node.."@"..from_host then
+								if item.attr.subscription == "remove" then
+									if session.roster[item.attr.jid] then
+										local success, err_type, err_cond, err_msg = rm_remove_from_roster(session, item.attr.jid);
+										if success then
+											session.send(st.reply(stanza));
+											rm_roster_push(from_node, from_host, item.attr.jid);
+										else
+											session.send(st.error_reply(stanza, err_type, err_cond, err_msg));
+										end
+									else
+										session.send(st.error_reply(stanza, "modify", "item-not-found"));
+									end
+								else
+									local r_item = {name = item.attr.name, groups = {}};
+									if r_item.name == "" then r_item.name = nil; end
+									if session.roster[item.attr.jid] then
+										r_item.subscription = session.roster[item.attr.jid].subscription;
+										r_item.ask = session.roster[item.attr.jid].ask;
+									else
+										r_item.subscription = "none";
+									end
+									for _, child in ipairs(item) do	
+										if child.name == "group" then
+											local text = t_concat(child);
+											if text and text ~= "" then
+												r_item.groups[text] = true;
+											end
+										end
+									end
+									local success, err_type, err_cond, err_msg = rm_add_to_roster(session, item.attr.jid, r_item);
+									if success then
+										session.send(st.reply(stanza));
+										rm_roster_push(from_node, from_host, item.attr.jid);
+									else
+										session.send(st.error_reply(stanza, err_type, err_cond, err_msg));
+									end
+								end
+							else
+								session.send(st.error_reply(stanza, "cancel", "not-allowed"));
+							end
+						else
+							session.send(st.error_reply(stanza, "modify", "bad-request")); -- FIXME what's the correct error?
+						end
+					else
+						session.send(st.error_reply(stanza, "modify", "bad-request"));
+					end
+					return true;
+				end
+			end
+		end);
