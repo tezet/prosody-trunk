c94b09a57e0a336c847ed1758b94da56d3777596
Waqas Hussain <waqas20@gmail.com>
2009-03-04 22:10:27 +0500

    Added util/xmlrpc.lua - functions for converting between XML-RPC stanzas and Lua objects

---
+++ plugins/mod_ping.lua
@@ -0,0 +1,20 @@
+-- Prosody IM v0.3
+-- Copyright (C) 2008-2009 Matthew Wild
+-- Copyright (C) 2008-2009 Waqas Hussain
+-- 
+-- This project is MIT/X11 licensed. Please see the
+-- COPYING file in the source package for more information.
+--
+
+
+
+local st = require "util.stanza";
+
+module:add_feature("urn:xmpp:ping");
+
+module:add_iq_handler({"c2s", "s2sin"}, "urn:xmpp:ping",
+	function(session, stanza)
+		if stanza.attr.type == "get" then
+			session.send(st.reply(stanza));
+		end
+	end);
