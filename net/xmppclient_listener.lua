5f116377498bb6a6715b604f6ebafb5afea5c000
Waqas Hussain <waqas20@gmail.com>
2009-05-15 23:42:35 +0500

    stanza_router: Fire events after all checks

---
+++ net/xmppclient_listener.lua
@@ -0,0 +1,141 @@
+-- Prosody IM v0.4
+-- Copyright (C) 2008-2009 Matthew Wild
+-- Copyright (C) 2008-2009 Waqas Hussain
+-- 
+-- This project is MIT/X11 licensed. Please see the
+-- COPYING file in the source package for more information.
+--
+
+
+
+local logger = require "logger";
+local log = logger.init("xmppclient_listener");
+local lxp = require "lxp"
+local init_xmlhandlers = require "core.xmlhandlers"
+local sm_new_session = require "core.sessionmanager".new_session;
+
+local connlisteners_register = require "net.connlisteners".register;
+
+local t_insert = table.insert;
+local t_concat = table.concat;
+local t_concatall = function (t, sep) local tt = {}; for _, s in ipairs(t) do t_insert(tt, tostring(s)); end return t_concat(tt, sep); end
+local m_random = math.random;
+local format = string.format;
+local sessionmanager = require "core.sessionmanager";
+local sm_new_session, sm_destroy_session = sessionmanager.new_session, sessionmanager.destroy_session;
+local sm_streamopened = sessionmanager.streamopened;
+local sm_streamclosed = sessionmanager.streamclosed;
+local st = require "util.stanza";
+
+local stream_callbacks = { stream_tag = "http://etherx.jabber.org/streams|stream", 
+		default_ns = "jabber:client",
+		streamopened = sm_streamopened, streamclosed = sm_streamclosed, handlestanza = core_process_stanza };
+
+function stream_callbacks.error(session, error, data)
+	if error == "no-stream" then
+		session.log("debug", "Invalid opening stream header");
+		session:close("invalid-namespace");
+	elseif session.close then
+		(session.log or log)("debug", "Client XML parse error: %s", tostring(error));
+		session:close("xml-not-well-formed");
+	end
+end
+
+local function handleerr(err) log("error", "Traceback[c2s]: %s: %s", tostring(err), debug.traceback()); end
+function stream_callbacks.handlestanza(a, b)
+	xpcall(function () core_process_stanza(a, b) end, handleerr);
+end
+
+local sessions = {};
+local xmppclient = { default_port = 5222, default_mode = "*a" };
+
+-- These are session methods --
+
+local function session_reset_stream(session)
+	-- Reset stream
+		local parser = lxp.new(init_xmlhandlers(session, stream_callbacks), "|");
+		session.parser = parser;
+		
+		session.notopen = true;
+		
+		function session.data(conn, data)
+			local ok, err = parser:parse(data);
+			if ok then return; end
+			session:close("xml-not-well-formed");
+		end
+		
+		return true;
+end
+
+
+local stream_xmlns_attr = {xmlns='urn:ietf:params:xml:ns:xmpp-streams'};
+local function session_close(session, reason)
+	local log = session.log or log;
+	if session.conn then
+		if reason then
+			if type(reason) == "string" then -- assume stream error
+				log("info", "Disconnecting client, <stream:error> is: %s", reason);
+				session.send(st.stanza("stream:error"):tag(reason, {xmlns = 'urn:ietf:params:xml:ns:xmpp-streams' }));
+			elseif type(reason) == "table" then
+				if reason.condition then
+					local stanza = st.stanza("stream:error"):tag(reason.condition, stream_xmlns_attr):up();
+					if reason.text then
+						stanza:tag("text", stream_xmlns_attr):text(reason.text):up();
+					end
+					if reason.extra then
+						stanza:add_child(reason.extra);
+					end
+					log("info", "Disconnecting client, <stream:error> is: %s", tostring(stanza));
+					session.send(stanza);
+				elseif reason.name then -- a stanza
+					log("info", "Disconnecting client, <stream:error> is: %s", tostring(reason));
+					session.send(reason);
+				end
+			end
+		end
+		session.send("</stream:stream>");
+		session.conn.close();
+		xmppclient.disconnect(session.conn, "stream error");
+	end
+end
+
+
+-- End of session methods --
+
+function xmppclient.listener(conn, data)
+	local session = sessions[conn];
+	if not session then
+		session = sm_new_session(conn);
+		sessions[conn] = session;
+
+		-- Logging functions --
+
+		local conn_name = "c2s"..tostring(conn):match("[a-f0-9]+$");
+		session.log = logger.init(conn_name);
+		
+		session.log("info", "Client connected");
+		
+		session.reset_stream = session_reset_stream;
+		session.close = session_close;
+		
+		session_reset_stream(session); -- Initialise, ready for use
+		
+		session.dispatch_stanza = stream_callbacks.handlestanza;
+	end
+	if data then
+		session.data(conn, data);
+	end
+end
+	
+function xmppclient.disconnect(conn, err)
+	local session = sessions[conn];
+	if session then
+		(session.log or log)("info", "Client disconnected: %s", err);
+		sm_destroy_session(session);
+		sessions[conn]  = nil;
+		session = nil;
+		collectgarbage("collect");
+	end
+end
+
+connlisteners_register("xmppclient", xmppclient);
---
+++ plugins/mod_register.lua
@@ -0,0 +1,160 @@
+-- Prosody IM v0.4
+-- Copyright (C) 2008-2009 Matthew Wild
+-- Copyright (C) 2008-2009 Waqas Hussain
+-- 
+-- This project is MIT/X11 licensed. Please see the
+-- COPYING file in the source package for more information.
+--
+
+
+local hosts = _G.hosts;
+local st = require "util.stanza";
+local config = require "core.configmanager";
+local datamanager = require "util.datamanager";
+local usermanager_user_exists = require "core.usermanager".user_exists;
+local usermanager_create_user = require "core.usermanager".create_user;
+local datamanager_store = require "util.datamanager".store;
+local os_time = os.time;
+local nodeprep = require "util.encodings".stringprep.nodeprep;
+
+module:add_feature("jabber:iq:register");
+
+module:add_iq_handler("c2s", "jabber:iq:register", function (session, stanza)
+	if stanza.tags[1].name == "query" then
+		local query = stanza.tags[1];
+		if stanza.attr.type == "get" then
+			local reply = st.reply(stanza);
+			reply:tag("query", {xmlns = "jabber:iq:register"})
+				:tag("registered"):up()
+				:tag("username"):text(session.username):up()
+				:tag("password"):up();
+			session.send(reply);
+		elseif stanza.attr.type == "set" then
+			if query.tags[1] and query.tags[1].name == "remove" then
+				-- TODO delete user auth data, send iq response, kick all user resources with a <not-authorized/>, delete all user data
+				local username, host = session.username, session.host;
+				--session.send(st.error_reply(stanza, "cancel", "not-allowed"));
+				--return;
+				usermanager_create_user(username, nil, host); -- Disable account
+				-- FIXME the disabling currently allows a different user to recreate the account
+				-- we should add an in-memory account block mode when we have threading
+				session.send(st.reply(stanza));
+				local roster = session.roster;
+				for _, session in pairs(hosts[host].sessions[username].sessions) do -- disconnect all resources
+					session:close({condition = "not-authorized", text = "Account deleted"});
+				end
+				-- TODO datamanager should be able to delete all user data itself
+				datamanager.store(username, host, "roster", nil);
+				datamanager.store(username, host, "vcard", nil);
+				datamanager.store(username, host, "private", nil);
+				datamanager.store(username, host, "offline", nil);
+				--local bare = username.."@"..host;
+				for jid, item in pairs(roster) do
+					if jid ~= "pending" then
+						if item.subscription == "both" or item.subscription == "to" then
+							-- TODO unsubscribe
+						end
+						if item.subscription == "both" or item.subscription == "from" then
+							-- TODO unsubscribe
+						end
+					end
+				end
+				datamanager.store(username, host, "accounts", nil); -- delete accounts datastore at the end
+			else
+				local username = query:child_with_name("username");
+				local password = query:child_with_name("password");
+				if username and password then
+					-- FIXME shouldn't use table.concat
+					username = nodeprep(table.concat(username));
+					password = table.concat(password);
+					if username == session.username then
+						if usermanager_create_user(username, password, session.host) then -- password change -- TODO is this the right way?
+							session.send(st.reply(stanza));
+						else
+							-- TODO unable to write file, file may be locked, etc, what's the correct error?
+							session.send(st.error_reply(stanza, "wait", "internal-server-error"));
+						end
+					else
+						session.send(st.error_reply(stanza, "modify", "bad-request"));
+					end
+				else
+					session.send(st.error_reply(stanza, "modify", "bad-request"));
+				end
+			end
+		end
+	else
+		session.send(st.error_reply(stanza, "cancel", "service-unavailable"));
+	end;
+end);
+
+local recent_ips = {};
+local min_seconds_between_registrations = config.get(module.host, "core", "min_seconds_between_registrations");
+local whitelist_only = config.get(module.host, "core", "whitelist_registration_only");
+local whitelisted_ips = config.get(module.host, "core", "registration_whitelist") or { "127.0.0.1" };
+local blacklisted_ips = config.get(module.host, "core", "registration_blacklist") or {};
+
+for _, ip in ipairs(whitelisted_ips) do whitelisted_ips[ip] = true; end
+for _, ip in ipairs(blacklisted_ips) do blacklisted_ips[ip] = true; end
+
+module:add_iq_handler("c2s_unauthed", "jabber:iq:register", function (session, stanza)
+	if config.get(module.host, "core", "allow_registration") == false then
+		session.send(st.error_reply(stanza, "cancel", "service-unavailable"));
+	elseif stanza.tags[1].name == "query" then
+		local query = stanza.tags[1];
+		if stanza.attr.type == "get" then
+			local reply = st.reply(stanza);
+			reply:tag("query", {xmlns = "jabber:iq:register"})
+				:tag("instructions"):text("Choose a username and password for use with this service."):up()
+				:tag("username"):up()
+				:tag("password"):up();
+			session.send(reply);
+		elseif stanza.attr.type == "set" then
+			if query.tags[1] and query.tags[1].name == "remove" then
+				session.send(st.error_reply(stanza, "auth", "registration-required"));
+			else
+				local username = query:child_with_name("username");
+				local password = query:child_with_name("password");
+				if username and password then
+					-- Check that the user is not blacklisted or registering too often
+					if blacklisted_ips[session.ip] or (whitelist_only and not whitelisted_ips[session.ip]) then
+							session.send(st.error_reply(stanza, "cancel", "not-acceptable"));
+							return;
+					elseif min_seconds_between_registrations and not whitelisted_ips[session.ip] then
+						if not recent_ips[session.ip] then
+							recent_ips[session.ip] = { time = os_time(), count = 1 };
+						else
+						
+							local ip = recent_ips[session.ip];
+							ip.count = ip.count + 1;
+							
+							if os_time() - ip.time < min_seconds_between_registrations then
+								ip.time = os_time();
+								session.send(st.error_reply(stanza, "cancel", "not-acceptable"));
+								return;
+							end
+							ip.time = os_time();
+						end
+					end
+					-- FIXME shouldn't use table.concat
+					username = nodeprep(table.concat(username));
+					password = table.concat(password);
+					if usermanager_user_exists(username, session.host) then
+						session.send(st.error_reply(stanza, "cancel", "conflict"));
+					else
+						if usermanager_create_user(username, password, session.host) then
+							session.send(st.reply(stanza)); -- user created!
+						else
+							-- TODO unable to write file, file may be locked, etc, what's the correct error?
+							session.send(st.error_reply(stanza, "wait", "internal-server-error"));
+						end
+					end
+				else
+					session.send(st.error_reply(stanza, "modify", "not-acceptable"));
+				end
+			end
+		end
+	else
+		session.send(st.error_reply(stanza, "cancel", "service-unavailable"));
+	end;
+end);
+
