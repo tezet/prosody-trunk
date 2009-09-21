2261a1768eab77e04f801ddf268af8a903eb52fc
Waqas Hussain <waqas20@gmail.com>
2009-09-22 00:38:06 +0500

    MUC: Added MUC feature to the disco#info replies of rooms.

---
+++ plugins/mod_httpserver.lua
@@ -0,0 +1,61 @@
+-- Prosody IM
+-- Copyright (C) 2008-2009 Matthew Wild
+-- Copyright (C) 2008-2009 Waqas Hussain
+-- 
+-- This project is MIT/X11 licensed. Please see the
+-- COPYING file in the source package for more information.
+--
+
+
+local httpserver = require "net.httpserver";
+
+local open = io.open;
+local t_concat = table.concat;
+
+local http_base = "www_files";
+
+local response_400 = { status = "400 Bad Request", body = "<h1>Bad Request</h1>Sorry, we didn't understand your request :(" };
+local response_404 = { status = "404 Not Found", body = "<h1>Page Not Found</h1>Sorry, we couldn't find what you were looking for :(" };
+
+local function preprocess_path(path)
+	if path:sub(1,1) ~= "/" then
+		path = "/"..path;
+	end
+	local level = 0;
+	for component in path:gmatch("([^/]+)/") do
+		if component == ".." then
+			level = level - 1;
+		elseif component ~= "." then
+			level = level + 1;
+		end
+		if level < 0 then
+			return nil;
+		end
+	end
+	return path;
+end
+
+function serve_file(path)
+	local f, err = open(http_base..path, "r");
+	if not f then return response_404; end
+	local data = f:read("*a");
+	f:close();
+	return data;
+end
+
+local function handle_file_request(method, body, request)
+	local path = preprocess_path(request.url.path);
+	if not path then return response_400; end
+	path = path:gsub("^/[^/]+", ""); -- Strip /files/
+	return serve_file(path);
+end
+
+local function handle_default_request(method, body, request)
+	local path = preprocess_path(request.url.path);
+	if not path then return response_400; end
+	return serve_file(path);
+end
+
+local ports = config.get(module.host, "core", "http_ports") or { 5280 };
+httpserver.set_default_handler(handle_default_request);
+httpserver.new_from_config(ports, "files", handle_file_request);
---
+++ plugins/mod_legacyauth.lua
@@ -0,0 +1,65 @@
+-- Prosody IM
+-- Copyright (C) 2008-2009 Matthew Wild
+-- Copyright (C) 2008-2009 Waqas Hussain
+-- 
+-- This project is MIT/X11 licensed. Please see the
+-- COPYING file in the source package for more information.
+--
+
+
+
+local st = require "util.stanza";
+local t_concat = table.concat;
+
+local secure_auth_only = module:get_option("require_encryption");
+
+local sessionmanager = require "core.sessionmanager";
+local usermanager = require "core.usermanager";
+
+module:add_feature("jabber:iq:auth");
+module:add_event_hook("stream-features", function (session, features)
+	if secure_auth_only and not session.secure then
+		-- Sorry, not offering to insecure streams!
+		return;
+	elseif not session.username then
+		features:tag("auth", {xmlns='http://jabber.org/features/iq-auth'}):up();
+	end
+end);
+
+module:add_iq_handler("c2s_unauthed", "jabber:iq:auth", 
+		function (session, stanza)
+			if secure_auth_only and not session.secure then
+				session.send(st.error_reply(stanza, "modify", "not-acceptable", "Encryption (SSL or TLS) is required to connect to this server"));
+				return true;
+			end
+			
+			local username = stanza.tags[1]:child_with_name("username");
+			local password = stanza.tags[1]:child_with_name("password");
+			local resource = stanza.tags[1]:child_with_name("resource");
+			if not (username and password and resource) then
+				local reply = st.reply(stanza);
+				session.send(reply:query("jabber:iq:auth")
+					:tag("username"):up()
+					:tag("password"):up()
+					:tag("resource"):up());
+			else
+				username, password, resource = t_concat(username), t_concat(password), t_concat(resource);
+				local reply = st.reply(stanza);
+				if usermanager.validate_credentials(session.host, username, password) then
+					-- Authentication successful!
+					local success, err = sessionmanager.make_authenticated(session, username);
+					if success then
+						local err_type, err_msg;
+						success, err_type, err, err_msg = sessionmanager.bind_resource(session, resource);
+						if not success then
+							session.send(st.error_reply(stanza, err_type, err, err_msg));
+							return true; -- FIXME need to unauthenticate here
+						end
+					end
+					session.send(st.reply(stanza));
+				else
+					session.send(st.error_reply(stanza, "auth", "not-authorized"));
+				end
+			end
+			return true;
+		end);
