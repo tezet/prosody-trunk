8e00137ea7bf73e8b351df480525a1d0fdf82f78
Matthew Wild <mwild1@gmail.com>
2009-12-02 18:02:47 +0000

    net.httpserver: Quick fix to set the correct Content-Type on simple (string) responses, for a few known types

---
+++ plugins/mod_version.lua
@@ -0,0 +1,41 @@
+-- Prosody IM
+-- Copyright (C) 2008-2009 Matthew Wild
+-- Copyright (C) 2008-2009 Waqas Hussain
+-- 
+-- This project is MIT/X11 licensed. Please see the
+-- COPYING file in the source package for more information.
+--
+
+local st = require "util.stanza";
+
+module:add_feature("jabber:iq:version");
+
+local version = "the best operating system ever!";
+
+if not module:get_option("hide_os_type") then
+	if os.getenv("WINDIR") then
+		version = "Windows";
+	else
+		local uname = io.popen("uname");
+		if uname then
+			version = uname:read("*a");
+		else
+			version = "an OS";
+		end
+	end
+end
+
+version = version:match("^%s*(.-)%s*$") or version;
+
+local query = st.stanza("query", {xmlns = "jabber:iq:version"})
+	:tag("name"):text("Prosody"):up()
+	:tag("version"):text(prosody.version):up()
+	:tag("os"):text(version);
+
+module:hook("iq/host/jabber:iq:version:query", function(event)
+	local stanza = event.stanza;
+	if stanza.attr.type == "get" and stanza.attr.to == module.host then
+		event.origin.send(st.reply(stanza):add_child(query));
+		return true;
+	end
+end);
