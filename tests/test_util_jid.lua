00f44b1fd4fadebbd20aa25c0b9e2e9275aef92e
Matthew Wild <mwild1@gmail.com>
2010-05-21 19:44:31 +0100

    Backed out changeset c75c22c316d6 (the XMPP spec now gives a better way to do this)

---
+++ tests/test_util_jid.lua
@@ -0,0 +1,56 @@
+-- Prosody IM
+-- Copyright (C) 2008-2009 Matthew Wild
+-- Copyright (C) 2008-2009 Waqas Hussain
+-- 
+-- This project is MIT/X11 licensed. Please see the
+-- COPYING file in the source package for more information.
+--
+
+function join(join)
+	assert_equal(join("a", "b", "c"), "a@b/c", "builds full JID");
+	assert_equal(join("a", "b", nil), "a@b", "builds bare JID");
+	assert_equal(join(nil, "b", "c"), "b/c", "builds full host JID");
+	assert_equal(join(nil, "b", nil), "b", "builds bare host JID");
+	assert_equal(join(nil, nil, nil), nil, "invalid JID is nil");
+	assert_equal(join("a", nil, nil), nil, "invalid JID is nil");
+	assert_equal(join(nil, nil, "c"), nil, "invalid JID is nil");
+	assert_equal(join("a", nil, "c"), nil, "invalid JID is nil");
+end
+
+
+function split(split)
+	function test(input_jid, expected_node, expected_server, expected_resource)
+		local rnode, rserver, rresource = split(input_jid);
+		assert_equal(expected_node, rnode, "split("..tostring(input_jid)..") failed");
+		assert_equal(expected_server, rserver, "split("..tostring(input_jid)..") failed");
+		assert_equal(expected_resource, rresource, "split("..tostring(input_jid)..") failed");
+	end
+	test("node@server", 		"node", "server", nil		);
+	test("node@server/resource", 	"node", "server", "resource"	);
+	test("server", 			nil, 	"server", nil		);
+	test("server/resource", 	nil, 	"server", "resource"	);
+	test(nil,			nil,	nil	, nil		);
+
+	test("node@/server", nil, nil, nil , nil );
+	test("@server",      nil, nil, nil , nil );
+	test("@server/resource",nil,nil,nil, nil );
+end
+
+function bare(bare)
+	assert_equal(bare("user@host"), "user@host", "bare JID remains bare");
+	assert_equal(bare("host"), "host", "Host JID remains host");
+	assert_equal(bare("host/resource"), "host", "Host JID with resource becomes host");
+	assert_equal(bare("user@host/resource"), "user@host", "user@host JID with resource becomes user@host");
+	assert_equal(bare("user@/resource"), nil, "invalid JID is nil");
+	assert_equal(bare("@/resource"), nil, "invalid JID is nil");
+	assert_equal(bare("@/"), nil, "invalid JID is nil");
+	assert_equal(bare("/"), nil, "invalid JID is nil");
+	assert_equal(bare(""), nil, "invalid JID is nil");
+	assert_equal(bare("@"), nil, "invalid JID is nil");
+	assert_equal(bare("user@"), nil, "invalid JID is nil");
+	assert_equal(bare("user@@"), nil, "invalid JID is nil");
+	assert_equal(bare("user@@host"), nil, "invalid JID is nil");
+	assert_equal(bare("user@@host/resource"), nil, "invalid JID is nil");
+	assert_equal(bare("user@host/"), nil, "invalid JID is nil");
+end
+
---
+++ util/pluginloader.lua
@@ -0,0 +1,69 @@
+-- Prosody IM
+-- Copyright (C) 2008-2009 Matthew Wild
+-- Copyright (C) 2008-2009 Waqas Hussain
+-- 
+-- This project is MIT/X11 licensed. Please see the
+-- COPYING file in the source package for more information.
+--
+
+
+local plugin_dir = CFG_PLUGINDIR or "./plugins/";
+
+local io_open, os_time = io.open, os.time;
+local loadstring, pairs = loadstring, pairs;
+
+local datamanager = require "util.datamanager";
+
+module "pluginloader"
+
+local function load_from_datastore(name)
+	local content = datamanager.load(name, nil, "plugins");
+	if not content or not content[1] then return nil, "Resource not found"; end
+	return content[1], name;
+end
+
+local function load_file(name)
+	local file, err = io_open(plugin_dir..name);
+	if not file then return file, err; end
+	local content = file:read("*a");
+	file:close();
+	return content, name;
+end
+
+function load_resource(plugin, resource, loader)
+	if not resource then
+		resource = "mod_"..plugin..".lua";
+	end
+	loader = loader or load_file;
+
+	local content, err = loader(plugin.."/"..resource);
+	if not content then content, err = loader(resource); end
+	-- TODO add support for packed plugins
+	
+	if not content and loader == load_file then
+		return load_resource(plugin, resource, load_from_datastore);
+	end
+	
+	return content, err;
+end
+
+function store_resource(plugin, resource, content, metadata)
+	if not resource then
+		resource = "mod_"..plugin..".lua";
+	end
+	local store = { content };
+	if metadata then
+		for k,v in pairs(metadata) do
+			store[k] = v;
+		end
+	end
+	datamanager.store(plugin.."/"..resource, nil, "plugins", store);
+end
+
+function load_code(plugin, resource)
+	local content, err = load_resource(plugin, resource);
+	if not content then return content, err; end
+	return loadstring(content, "@"..err);
+end
+
+return _M;
