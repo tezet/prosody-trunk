aad74a66f0b372bdc91926bce167a36e322d6537
Kim Alvefur <zash@zash.se>
2012-09-19 15:12:18 +0200

    mod_admin_adhoc: Add commands for activating and deactivating hosts

---
+++ plugins/mod_storage_internal.lua
@@ -0,0 +1,27 @@
+local datamanager = require "core.storagemanager".olddm;
+
+local host = module.host;
+
+local driver = {};
+local driver_mt = { __index = driver };
+
+function driver:open(store)
+	return setmetatable({ store = store }, driver_mt);
+end
+function driver:get(user)
+	return datamanager.load(user, host, self.store);
+end
+
+function driver:set(user, data)
+	return datamanager.store(user, host, self.store, data);
+end
+
+function driver:stores(username)
+	return datamanager.stores(username, host);
+end
+
+function driver:purge(user)
+	return datamanager.purge(user, host);
+end
+
+module:provides("storage", driver);
